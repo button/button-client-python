39f50ef2baa70e0681b87a05b69336c8fa79276b
Tyler Nappy <tylernappy@gmail.com>
2017-07-18 12:38:30 -0400

    added testing for /customers

--- pybutton/test/resources/customers_test.py
+++ pybutton/test/resources/customers_test.py
@@ -19,34 +19,37 @@ config = {
 
 class CustomersTestCase(TestCase):
 
-        def test_path(self):
-            customer = Customers('sk-XXX', config)
-            self.assertEqual(customer._path(), '/v1/customers')
-            self.assertEqual(customer._path('customer-1'), '/v1/customers/customer-1')
+    def test_path(self):
+        customer = Customers('sk-XXX', config)
+        self.assertEqual(customer._path(), '/v1/customers')
+        self.assertEqual(
+            customer._path('customer-1'),
+            '/v1/customers/customer-1'
+        )
 
-        def test_get(self):
-            customer = Customers('sk-XXX', config)
-            customer_response = {'a': 1}
+    def test_get(self):
+        customer = Customers('sk-XXX', config)
+        customer_response = {'a': 1}
 
-            api_get = Mock()
-            api_get.return_value = customer_response
+        api_get = Mock()
+        api_get.return_value = customer_response
 
-            with patch.object(customer, 'api_get', api_get):
-                response = customer.get('customer-XXX')
+        with patch.object(customer, 'api_get', api_get):
+            response = customer.get('customer-XXX')
 
-            self.assertEqual(response, customer_response)
-            api_get.assert_called_with('/v1/customers/customer-XXX')
+        self.assertEqual(response, customer_response)
+        api_get.assert_called_with('/v1/customers/customer-XXX')
 
-        def test_create(self):
-            customer = Customers('sk-XXX', config)
-            customer_payload = {'b': 2}
-            customer_response = {'a': 1}
+    def test_create(self):
+        customer = Customers('sk-XXX', config)
+        customer_payload = {'b': 2}
+        customer_response = {'a': 1}
 
-            api_post = Mock()
-            api_post.return_value = customer_response
+        api_post = Mock()
+        api_post.return_value = customer_response
 
-            with patch.object(customer, 'api_post', api_post):
-                response = customer.create(customer_payload)
+        with patch.object(customer, 'api_post', api_post):
+            response = customer.create(customer_payload)
 
-            self.assertEqual(response, customer_response)
-            api_post.assert_called_with('/v1/customers', customer_payload)
+        self.assertEqual(response, customer_response)
+        api_post.assert_called_with('/v1/customers', customer_payload)
