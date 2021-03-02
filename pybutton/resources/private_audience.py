cabbe12632b38277749ca81f89a6d05c571b2011
Sean Summers <seanjsummers@gmail.com>
2021-03-01 20:11:47 -0800

    consolidate methods

--- pybutton/resources/private_audience.py
+++ pybutton/resources/private_audience.py
@@ -10,42 +10,61 @@ except:
     pass
 
 from pybutton.resources.resource import Resource
+from pybutton.error import ButtonClientError
 
 
 class PrivateAudience(Resource):
 
+    def _validateEnvironment(self):
+        try:
+            pl
+        except:
+            raise ButtonClientError((
+                'pybloom_live is required for Button Private Audiences'
+            ))
+        if not self.config['private_audience_secret']:
+            raise ButtonClientError((
+                'Audience secret is not set in the config'
+            ))
+
+    def _cleanAndHashIdentifier(self, identifier):
+        try:
+            clean_identifier = identifier.encode('utf-8').lower().strip()
+            hashed_identifier = hashlib.sha256(clean_identifier)
+            hashed_identifier.update(
+                self.config['private_audience_secret'].encode('utf-8'))
+            return hashed_identifier.hexdigest()
+        except:
+            return None
+
     def load(self, filename):
+        self._validateEnvironment()
         with open(filename, 'rb') as fp:
             self.private_audience_loaded = pl.BloomFilter.fromfile(fp)
         return 'success'
 
     def evaluate(self, identifier):
-        identifier_clean = identifier.encode('utf-8').lower().strip()
-        hashed_identifier = hashlib.sha256(identifier_clean)
-        secret = self.config['private_audience_secret'].encode('utf-8')
-        hashed_identifier.update(secret)
-        return hashed_identifier.hexdigest() in self.private_audience_loaded
+        self._validateEnvironment()
+        hashed_id = self._cleanAndHashIdentifier(identifier)
+        return hashed_id in self.private_audience_loaded
 
     def match(self, match_list):
+        self._validateEnvironment()
         return_list = []
         for m in match_list:
-            m_clean = m.encode('utf-8').lower().strip()
-            hashed_identifier = hashlib.sha256(m_clean)
-            secret = self.config['private_audience_secret'].encode('utf-8')
-            hashed_identifier.update(secret)
-            if hashed_identifier.hexdigest() in self.private_audience_loaded:
+            hashed_id = self._cleanAndHashIdentifier(m)
+            if hashed_id in self.private_audience_loaded:
                 return_list.append(m)
         return return_list
 
     def create(self, audience_name, identifier_list, error_rt=0.00001):
+        self._validateEnvironment()
         # Initialize Bloom Filter
         f = pl.BloomFilter(capacity=len(identifier_list), error_rate=error_rt)
         for m in identifier_list:
-            m_clean = m.encode('utf-8').lower().strip()
-            hashed_identifier = hashlib.sha256(m_clean)
-            secret = self.config['private_audience_secret'].encode('utf-8')
-            hashed_identifier.update(secret)
-            f.add(hashed_identifier.hexdigest())
+            hashed_id = self._cleanAndHashIdentifier(m)
+            if hashed_id:
+                f.add(hashed_id)
         with open(audience_name + '.buttonaudience', 'wb') as fp:
             f.tofile(fp)
         return 'success'
