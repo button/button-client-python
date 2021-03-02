abe4f389fd9c49c1adf69af31f400c63360c1965
Sean Summers <seanjsummers@gmail.com>
2021-03-01 16:49:22 -0800

    import bloom filter library within try/except

--- pybutton/resources/private_audience.py
+++ pybutton/resources/private_audience.py
@@ -2,8 +2,12 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
-import pybloom_live as pl
 import hashlib
+
+try:
+    import pybloom_live as pl
+except:
+    pass
 
 from pybutton.resources.resource import Resource
 
